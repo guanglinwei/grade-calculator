2180f9bb4e10cca94495be3999609c0ec9cb75ea
Guang-Lin Wei <guanglinwei@gmail.com>
2022-03-29 20:34:54 -0400

    added summary percentage

--- src/app/shared/percent-display-pipe.ts
+++ src/app/shared/percent-display-pipe.ts
@@ -3,7 +3,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: "percentDisplay" })
 
 export class PercentDisplayPipe implements PipeTransform {
-    transform(num: number, denom: number): string {
-        return denom === 0 ? "0.00" : (100 * num / denom).toFixed(2).toString();
+    transform(num: number | string, denom: number | string): string {
+        let n = parseFloat(num.toString());
+        let d = parseFloat(denom.toString());
+        return d === 0 ? "0.00" : (100 * n / d).toFixed(2).toString();
     }
 }
